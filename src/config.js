1b788fe79354849fe2c32134f1d6aaf1e3c2fa03
MD. Ariful Alam <swazan.arif@gmail.com>
2021-10-18 22:01:40 +0600

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,39 @@
+// config.js
+module.exports = {
+    themeConfig: {
+        default: 'light',
+
+        // Hides the switch in the navbar
+        // Useful if you want to support a single color mode
+        disableSwitch: false,
+
+        // Should we use the prefers-color-scheme media-query,
+        // using user system preferences, instead of the hardcoded default
+        respectPrefersColorScheme: true,
+
+        // Available themes. To remove any theme, exclude from here.
+        themes: [
+            'light',
+            'dark',
+            'cupcake',
+            'bumblebee',
+            'emerald',
+            'corporate',
+            'synthwave',
+            'retro',
+            'cyberpunk',
+            'valentine',
+            'halloween',
+            'garden',
+            'forest',
+            'aqua',
+            'lofi',
+            'pastel',
+            'fantasy',
+            'wireframe',
+            'black',
+            'luxury',
+            'dracula'
+        ]
+    }
+}
