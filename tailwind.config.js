6a1adf3662a3302e09f88f2c6e2451d6ffb25199
MD. Ariful Alam <swazan.arif@gmail.com>
2021-10-20 18:14:10 +0600

    Add safelist while purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    purge: {
+        content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+        options: {
+            safelist: [
+                'hidden',
+                'border-base-200',
+                'bg-base-200',
+                'text-base-content',
+            ],
+        },
+    },
     darkMode: false,
     theme: {
         extend: {},
