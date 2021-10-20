157282bedf794a7cea93840414a66561c70a6bb8
MD. Ariful Alam <swazan.arif@gmail.com>
2021-10-20 18:26:09 +0600

    Fix purge issue of navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-    purge: {
-        content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-        options: {
-            safelist: [
-                'hidden',
-                'border-base-200',
-                'bg-base-200',
-                'text-base-content',
-            ],
-        },
-    },
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false,
     theme: {
         extend: {},
