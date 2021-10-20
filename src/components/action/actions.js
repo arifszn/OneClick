ce114965c72bdfb5c16d6e0446e11638b98bbf7d
Ariful Alam <swazan.arif@gmail.com>
2021-10-21 02:22:48 +0600

    Update actions.js

--- src/components/action/actions.js
+++ src/components/action/actions.js
@@ -105,13 +105,13 @@ export const actions = [
     {
         name: 'Angular.js',
         icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width={iconSize} height={iconSize}><path fill="#bdbdbd" d="M23.933 2L3 9.285 6.308 36.408 23.955 46 41.693 36.278 45 9.156z"/><path fill="#b71c1c" d="M42.818 10.527L24 4.135 24 43.695 39.832 35.017z"/><path fill="#dd2c00" d="M23.941 4.115L5.181 10.644 8.168 35.143 23.951 43.721 24 43.695 24 4.135z"/><path fill="#bdbdbd" d="M24 5.996L24 15.504 32.578 34 36.987 34z"/><path fill="#eee" d="M11.013 34L15.422 34 24 15.504 24 5.996z"/><path fill="#bdbdbd" d="M24 24H30V28H24z"/><path fill="#eee" d="M18 24H24V28H18z"/></svg>,
-        description: "Create a new Angular project online CodeSandbox.",
+        description: "Create a new Angular project online with CodeSandbox.",
         url: 'https://ng.new'
     },
     {
         name: 'Node.js',
         icon: <FaNode size={iconSize} color='#5C9553'/>,
-        description: "Create a new Node.js project online CodeSandbox.",
+        description: "Create a new Node.js project online with CodeSandbox.",
         url: 'https://nodejs.new'
     },
     {
@@ -147,7 +147,7 @@ export const actions = [
     {
         name: 'Svelte',
         icon: <SiSvelte size={iconSize} color='#FF3E00'/>,
-        description: "Create a new Svelte project using CodeSandbox.",
+        description: "Create a new Svelte project online with CodeSandbox.",
         url: 'https://svelte.new'
     },
     {
@@ -159,7 +159,7 @@ export const actions = [
     {
         name: 'Glitch',
         icon: <SiGlitch size={iconSize} color='#3333FF'/>,
-        description: "Create a full-stack apps in your browser and collaborate live.",
+        description: "Create a full-stack app in your browser and collaborate live.",
         url: 'https://glitch.new'
     },
     {
@@ -184,7 +184,7 @@ export const actions = [
     {
         name: 'Miro',
         icon: <SiMiro size={iconSize} />,
-        description: "Create wireframes for your project using Miro.",
+        description: "Create a wireframe for your project using Miro.",
         url: 'https://wireframe.new'
     },
     {
