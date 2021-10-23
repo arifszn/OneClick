16ba9dcd18f1638f804f8b5bcafdcbb73485b037
MD. Ariful Alam <swazan.arif@gmail.com>
2021-10-23 14:11:33 +0600

    Add padding for large device

--- src/components/action/ActionCard.js
+++ src/components/action/ActionCard.js
@@ -26,7 +26,7 @@ const ActionCard = ({ action }) => {
                             window.open(action.url, '_blank');
                         }}
                     >
-                        <svg className="p-2" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 12 16" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"></path></svg>
+                        <svg className="p-2 lg:p-3" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 12 16" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"></path></svg>
                     </button>
                 </div>
             </div>
