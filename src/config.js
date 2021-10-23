e4ee89b3b07ab9be7bf8c2d9cf2614ce98711ddb
MD. Ariful Alam <swazan.arif@gmail.com>
2021-10-23 13:34:41 +0600

    Remove comment

--- src/config.js
+++ src/config.js
@@ -2,16 +2,8 @@
 module.exports = {
     themeConfig: {
         default: 'cupcake',
-
-        // Hides the switch in the navbar
-        // Useful if you want to support a single color mode
         disableSwitch: false,
-
-        // Should we use the prefers-color-scheme media-query,
-        // using user system preferences, instead of the hardcoded default
         respectPrefersColorScheme: true,
-
-        // Available themes. To remove any theme, exclude from here.
         themes: [
             'light',
             'dark',
