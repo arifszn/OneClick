876f0d039e771b7c2dc24f58fcc0fb89ee6fd0ee
Ariful Alam <swazan.arif@gmail.com>
2022-07-16 12:46:26 +0600

    Inject style from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          injectStyle: ``,
+        },
+      },
+    }),
+  ],
+});
