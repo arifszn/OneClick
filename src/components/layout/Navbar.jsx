81c716a19d62cdcff80da380e20778d36fe98e1b
Ariful Alam <swazan.arif@gmail.com>
2023-01-15 16:33:58 +0600

    Change theme changer style

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,11 @@
 import { useState, useRef, useEffect, useContext } from 'react';
 import { navigateToPath } from '../../helpers/utils';
-import { AiOutlineControl } from 'react-icons/ai';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import config from '../../../config';
 import PropTypes from 'prop-types';
+import { VscSymbolColor } from 'react-icons/vsc';
+import { TbPalette } from 'react-icons/tb';
+import { RiPaintFill } from 'react-icons/ri';
 
 const Navbar = ({ query, setQuery }) => {
   const [theme, setTheme] = useContext(ThemeContext);
@@ -91,8 +93,7 @@ const Navbar = ({ query, setQuery }) => {
         </div>
         <div title="Change Theme" className="dropdown dropdown-end">
           <div tabIndex={0} className="btn btn-ghost m-1 normal-case">
-            <AiOutlineControl className="inline-block w-5 h-5 stroke-current md:mr-2" />
-            <span className="hidden md:inline">Change Theme</span>
+            <RiPaintFill className="inline-block w-5 h-5 stroke-current" />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1792 1792"
@@ -122,23 +123,25 @@ const Navbar = ({ query, setQuery }) => {
             </ul>
           </div>
         </div>
-        <div title="Github" className="items-center flex-none">
-          <a
-            aria-label="Github"
-            target="_blank"
-            href="https://github.com/arifszn/awesome-shortcuts"
-            rel="noreferrer"
-            className="normal-case btn btn-ghost drawer-button btn-square"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              className="inline-block w-6 h-6 fill-current"
+        {!config.extension && (
+          <div title="Github" className="items-center flex-none">
+            <a
+              aria-label="Github"
+              target="_blank"
+              href="https://github.com/arifszn/awesome-shortcuts"
+              rel="noreferrer"
+              className="normal-case btn btn-ghost drawer-button btn-square"
             >
-              <path d="M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z" />
-            </svg>
-          </a>
-        </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="inline-block w-6 h-6 fill-current"
+              >
+                <path d="M256,32C132.3,32,32,134.9,32,261.7c0,101.5,64.2,187.5,153.2,217.9a17.56,17.56,0,0,0,3.8.4c8.3,0,11.5-6.1,11.5-11.4,0-5.5-.2-19.9-.3-39.1a102.4,102.4,0,0,1-22.6,2.7c-43.1,0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1,1.4-14.1h.1c22.5,2,34.3,23.8,34.3,23.8,11.2,19.6,26.2,25.1,39.6,25.1a63,63,0,0,0,25.6-6c2-14.8,7.8-24.9,14.2-30.7-49.7-5.8-102-25.5-102-113.5,0-25.1,8.7-45.6,23-61.6-2.3-5.8-10-29.2,2.2-60.8a18.64,18.64,0,0,1,5-.5c8.1,0,26.4,3.1,56.6,24.1a208.21,208.21,0,0,1,112.2,0c30.2-21,48.5-24.1,56.6-24.1a18.64,18.64,0,0,1,5,.5c12.2,31.6,4.5,55,2.2,60.8,14.3,16.1,23,36.6,23,61.6,0,88.2-52.4,107.6-102.3,113.3,8,7.1,15.2,21.1,15.2,42.5,0,30.7-.3,55.5-.3,63,0,5.4,3.1,11.5,11.4,11.5a19.35,19.35,0,0,0,4-.4C415.9,449.2,480,363.1,480,261.7,480,134.9,379.7,32,256,32Z" />
+              </svg>
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
