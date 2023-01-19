36797efb9ff654afc761e6db93398673c8ca2e2b
Ariful Alam <swazan.arif@gmail.com>
2023-01-19 17:23:47 +0600

    Make change changer component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,26 +1,8 @@
-import { useEffect, useContext } from 'react';
 import { navigateToPath } from '../../helpers/utils';
-import { ThemeContext } from '../../contexts/ThemeContext';
 import config from '../../../config';
-import { RiPaintFill } from 'react-icons/ri';
+import ThemeChanger from '../theme-changer';
 
 const Navbar = () => {
-  const [theme, setTheme] = useContext(ThemeContext);
-
-  useEffect(() => {
-    if (theme) {
-      document.documentElement.setAttribute('data-theme', theme);
-    }
-  }, [theme]);
-
-  const changeTheme = (e, selectedTheme) => {
-    e.preventDefault();
-    document.querySelector('html').setAttribute('data-theme', selectedTheme);
-    localStorage.setItem('awesome-shortcuts-theme', selectedTheme);
-
-    setTheme(selectedTheme);
-  };
-
   return (
     <div
       className={`inset-x-0 top-0 z-50 w-full transition duration-200 ease-in-out border-b border-transparent bg-transparent absolute ${
@@ -51,38 +33,7 @@ const Navbar = () => {
           </a>
         </div>
         <div className="flex-1" />
-        <div title="Change Theme" className="dropdown dropdown-end">
-          <div tabIndex={0} className="btn btn-ghost normal-case">
-            <RiPaintFill className="inline-block w-5 h-5 stroke-current" />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 1792 1792"
-              className="inline-block w-4 h-4 ml-1 fill-current"
-            >
-              <path d="M1395 736q0 13-10 23l-466 466q-10 10-23 10t-23-10l-466-466q-10-10-10-23t10-23l50-50q10-10 23-10t23 10l393 393 393-393q10-10 23-10t23 10l50 50q10 10 10 23z" />
-            </svg>
-          </div>
-          <div
-            tabIndex={0}
-            className="mt-16 overflow-y-auto shadow-2xl top-px dropdown-content h-96 w-52 rounded-b-box bg-base-200 text-base-content"
-          >
-            <ul className="p-4 menu compact">
-              {config.themeConfig.themes.map((item, index) => (
-                <li key={index}>
-                  {/* eslint-disable-next-line */}
-                  <a
-                    onClick={(e) => changeTheme(e, item)}
-                    className={`${theme === item ? 'active' : ''}`}
-                  >
-                    <span className="opacity-70 capitalize">
-                      {index === 0 ? 'Default' : item}
-                    </span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <ThemeChanger />
         {!config.extension && (
           <div title="Github" className="items-center flex-none">
             <a
